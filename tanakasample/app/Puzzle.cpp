0c241287e5579a7d83640d0a3f37e1779b5e2ed6
Hiroki Tachiyama <hirokitachiyama@gmail.com>
2016-09-21 05:49:25 +0900

    puzzleを追加

---
+++ tanakasample/app/Puzzle.cpp
@@ -0,0 +1,25 @@
+#include "ev3api.h"
+#include "ColorSensor.h"
+#include "Clock.h"
+
+#include "PidWalker.h"
+
+Puzzle::Puzzle():
+  sonarSensor(PORT_3), coloeSensor(PORT_2){}
+
+
+void Puzzle::doPuzzle{
+  
+}
+
+void Puzzle::runOneBlock{
+  
+}
+
+//円の中にたどり着いたら90度曲がる
+void Puzzle::turn90InCircle{
+  while(1) {}
+}
+
+
+
