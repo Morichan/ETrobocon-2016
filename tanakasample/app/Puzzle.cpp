e5b54ab50558292ccb0dfb99a89ecde5b7b5ce53
Morichan <nintendo698@gmai.com>
2016-09-22 01:40:21 +0900

    Rコースの難所ノード移動、前後左右はほぼ完ぺき

--- tanakasample/app/Puzzle.cpp
+++ tanakasample/app/Puzzle.cpp
@@ -7,44 +7,18 @@
 
 //none:0 black:1 blue:2 green:3 yellow:4 red:5 white:6 brown:7
 
-
 Puzzle::Puzzle():
     sonarSensor(PORT_3), colorSensor(PORT_2){
 }
 
-
 void Puzzle::doPuzzle(){
-    // pidWalker.walker.edgeChange();
+    pidWalker.walker.edgeChange();
     pidWalker.pid.setPid(0.4, 0.0, 2.0, 30);
-    pidWalker.setForward(15);
+    pidWalker.setForward(20);
 
     while(1){
 
-        //黒線
-        while(1){
-            pidWalker.trace();
-
-            /*
-             * 色を検知したら色の値をとって次に進む
-             * 1 = 黒, 2 = 青, 3 = 緑, 4 = 黄, 5 = 赤, 6 = 白
-             */
-            if(colorSensor.getColorNumber() == 2) {
-                currentCircleColor = 2;
-                break;
-            }
-            else if(colorSensor.getColorNumber() == 3) {
-                currentCircleColor = 3;
-                break;
-            }
-            else if(colorSensor.getColorNumber() == 4) {
-                currentCircleColor = 4;
-                break;
-            }
-            else if(colorSensor.getColorNumber() == 5) {
-                currentCircleColor = 5;
-                break;
-            }
-        }
+        goAheadNode();
 
         // while(1){
         //     pidWalker.stop();
@@ -55,11 +29,28 @@ void Puzzle::doPuzzle(){
         // }
 
         if(currentCircleColor == 5) {
-            walker.angleChange(45, 1);
-            break;
-        } else {
+            if(flag < 1) {
+                goLowLeftEdge();
+                flag++;
+            } else {
+                goUpLeftEdge();
+                flag++;
+            }
+        }
+        if(currentCircleColor == 4) {
+            if(flag < 3) {
+                goUpRightEdge();
+                flag++;
+            } else if(flag == 3) {
+                goLowRightEdge();
+                flag++;
+            }
+        }
+        if(currentCircleColor == 3) {
+            flag = 0;
             break;
         }
+
         // while (colorSensor.getColorNumber() != 1) {
         //     walker.run(10,0);
         //     flag = 1;
@@ -90,9 +81,104 @@ void Puzzle::doPuzzle(){
     }
 }
 
+void Puzzle::goAheadNode() {
 
+    //黒線
+    while(1){
+        pidWalker.trace();
 
+        /*
+         * 色を検知したら色の値をとって次に進む
+         * 1 = 黒, 2 = 青, 3 = 緑, 4 = 黄, 5 = 赤, 6 = 白
+         */
+        if(colorSensor.getColorNumber() == 2) {
+            currentCircleColor = 2;
+            break;
+        }
+        else if(colorSensor.getColorNumber() == 3) {
+            currentCircleColor = 3;
+            break;
+        }
+        else if(colorSensor.getColorNumber() == 4) {
+            currentCircleColor = 4;
+            break;
+        }
+        else if(colorSensor.getColorNumber() == 5) {
+            currentCircleColor = 5;
+            break;
+        }
+    }
+}
 
+void Puzzle::goLeftEdge() {
+    pidWalker.walker.angleChange(45, 1);
+    while (colorSensor.getColorNumber() != 1) {
+        pidWalker.walker.run(20,0);
+    }
+    pidWalker.walker.moveAngle(20, 20);
+}
+
+void Puzzle::goRightEdge() {
+    pidWalker.walker.angleChange(45, -1);
+    while (colorSensor.getColorNumber() != 1) {
+        pidWalker.walker.run(20,0);
+    }
+    pidWalker.accelerate(1, 20);
+}
+
+void Puzzle::goFrontEdge() {
+    pidWalker.walker.moveAngle(20, 150);
+}
+
+void Puzzle::goBackEdge() {
+    pidWalker.walker.moveAngle(20, 20);
+    pidWalker.walker.angleChange(90, 1);
+    pidWalker.walker.moveAngle(20, 40);
+    pidWalker.walker.angleChange(45, 1);
+    while(colorSensor.getColorNumber() != 1) {
+        pidWalker.walker.run(0, 10);
+    }
+}
+
+void Puzzle::goLowRightEdge() {
+    pidWalker.walker.angleChange(110, -1);
+    while(colorSensor.getColorNumber() != 1) {
+        pidWalker.walker.run(20, 0);
+    }
+    pidWalker.walker.angleChange(60, -1);
+    pidWalker.accelerate(1, 20);
+}
+
+void Puzzle::goUpRightEdge() {
+    pidWalker.walker.angleChange(60, -1);
+    pidWalker.walker.moveAngle(20, 360);
+    while(colorSensor.getColorNumber() != 1) {
+        pidWalker.walker.run(20, 0);
+    }
+    pidWalker.walker.moveAngle(20, 60);
+    pidWalker.walker.angleChange(45, 1);
+    pidWalker.accelerate(1, 20);
+}
+
+void Puzzle::goLowLeftEdge() {
+    pidWalker.walker.angleChange(95, 1);
+    while(colorSensor.getColorNumber() != 1) {
+        pidWalker.walker.run(20, 0);
+    }
+    pidWalker.walker.moveAngle(20, 60);
+    pidWalker.walker.angleChange(45, 1);
+    pidWalker.accelerate(1, 20);
+}
+
+void Puzzle::goUpLeftEdge() {
+    pidWalker.walker.angleChange(50, 1);
+    pidWalker.walker.moveAngle(20, 360);
+    while(colorSensor.getColorNumber() != 1) {
+        pidWalker.walker.run(20, 0);
+    }
+    pidWalker.walker.angleChange(45, -1);
+    pidWalker.accelerate(1, 20);
+}
 
 void Puzzle::runOneBlock(){
     //黒線
