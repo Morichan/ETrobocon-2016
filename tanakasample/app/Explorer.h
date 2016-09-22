2b6d2df7767667001addf45b4129f4f57a58ba04
Morichan <nintendo698@gmai.com>
2016-09-22 16:18:27 +0900

    任意のノードからノードへ移動できるようになった

--- tanakasample/app/Explorer.h
+++ tanakasample/app/Explorer.h
@@ -17,13 +17,17 @@ private:
 	vector<Node*> nodeList;
 	Node* startNode;
 	Node* goalNode;
+    vector<int> root;
 
 public:
 	Explorer();
 	void set(int start, int goal);
 	void print();
-	vector<int> search();
+	void search();
 	void setBlocks(int p1, int p2, int p3, int p4);
+    int getRoot(int i);
+    size_t getSize();
+    bool contains(int num);
 
 };
 #endif //INC_EXPLORER
